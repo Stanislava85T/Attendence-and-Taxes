0069316e9efc5262193269da7a80f7aa44006168
Stanislava Aleksieva-Pandova <stanislava.aleksieva@gmail.com>
2026-02-23 13:38:30 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = 'klub-v1';
+const ASSETS = [
+  '/Attendence-and-Taxes/',
+  '/Attendence-and-Taxes/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
